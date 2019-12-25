3256bfaf8a66d6abb6c6f9d17ad57c9f08ea69b3
zabusworkshop <eugeny.zabus@gmail.com>
2019-12-25 10:57:21 +0000

    initial state

---
+++ README.md
@@ -0,0 +1,1 @@
+# sap.training.v2.odata
---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/v2/ODataModel",
+	"sap/m/MessageBox"
+], function(Controller, ODataModel, MessageBox) {
+	"use strict";
+
+	return Controller.extend("sap.training.v2.odata.controller.Main", {
+
+		onInit: function() {
+		
+		}
+
+	});
+});
